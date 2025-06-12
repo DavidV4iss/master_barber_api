097c0bf7a205e9f0d3eb28b703ac95faddd47a60
DavidVaiss <cristianrueda0313@gmail.com>
2025-06-11 20:17:41 -0500

    que sera

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
 
