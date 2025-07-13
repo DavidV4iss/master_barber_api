3062d71ebfb88060ca242a11fb0d256c8e64797b
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-07-13 17:59:01 -0500

    Aver

--- server.js
+++ server.js
@@ -32,6 +32,15 @@ app.use(cors({
 }));
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+
 
 
 
