740d909376e43c3844e19972470ffbf935938ed9
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-07-13 18:34:54 -0500

    ya no mas

--- server.js
+++ server.js
@@ -28,12 +28,9 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}));
-
-
-
-
+}))
 
 
 
