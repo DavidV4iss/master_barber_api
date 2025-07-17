d90c61b5a77bdaa441bd9634077ab8438829f8b9
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-07-17 12:37:29 -0500

    Sever corregido

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }))
